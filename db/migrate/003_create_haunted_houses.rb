cf3aa44322148e0966282b3576f74af212d3f15c
Mason Phillips <mjphillips2000@gmail.com>
2020-12-28 21:06:18 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,13 +1,4 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-    create_table :CostumeStore do |s|
-      s.string :name
-      s.string :location
-      s.integer :costume_inventory
-      s.integer :employees
-      s.boolean :business
-      s.datetime :created_at
-      s.datetime :updated_at
-    end
   end
 end
