a4ba94d5a64781303f6ccef968c0000a18200962
Mason Phillips <mjphillips2000@gmail.com>
2020-12-28 21:06:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :CostumeStore do |s|
+      s.string :name
+      s.string :location
+      s.integer :costume_inventory
+      s.integer :employees
+      s.boolean :business
+      s.datetime :created_at
+      s.datetime :updated_at
+    end
+  end
+end
